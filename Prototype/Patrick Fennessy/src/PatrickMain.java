804b0b2497be2c6f71c665003de7d88ffeead052
pfen0005 <pfen0005@student.monash.edu>
2025-03-29 21:50:16 +1100

    finished mini prototype/project

--- Prototype/Patrick Fennessy/src/PatrickMain.java
+++ Prototype/Patrick Fennessy/src/PatrickMain.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class PatrickMain {
     public static void main(String[] args) {
         System.out.println("print me!");
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Patrick Fennessy");
+
+        window.setLocationRelativeTo(null); // window at center of screen
+        window.setVisible(true);
+
     }
 }
