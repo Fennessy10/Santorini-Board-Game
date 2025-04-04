8a7c4c5b5252cb8a88c7298e83aed9fdc77f0d48
jsee0010 <jsee0010@student.monash.edu>
2025-04-04 14:56:09 +1100

    update text for prototype

--- Prototype/Joshua J. Seeger/src/Main.java
+++ Prototype/Joshua J. Seeger/src/Main.java
@@ -7,11 +7,12 @@ public class Main {
     }
     public static void initFrame() {
         Frame f = new Frame();
-        Label l1 = new Label("Hello FIT3077 from group 25!");
+        Label l1 = new Label("Hello FIT3077 from group 24!");
         l1.setBounds(100, 50, 200, 80);
         f.add(l1);
         f.setSize(500, 500);
         f.setLayout(null);
         f.setVisible(true);
     }
+
 }
