a639f57bb0c855b978838555f67fc25793411a54
pfen0005 <pfen0005@student.monash.edu>
2025-04-29 13:11:00 +1000

    seperated and renamed classes to align with UML diagram

--- Prototype/Sprint 1 Workspace/Joshua J. Seeger/src/JoshuaMain.java
+++ Prototype/Sprint 1 Workspace/Joshua J. Seeger/src/JoshuaMain.java
@@ -1,7 +1,7 @@
 
 
 import java.awt.*;
-public class Main {
+public class JoshuaMain {
     public static void main(String[] args) {
         initFrame();
     }
--- Prototype/src/GamePanel.java
+++ Prototype/src/GamePanel.java
@@ -1,5 +1,22 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class GamePanel {
-    private JPanel panel2;
+public class GamePanel extends JPanel {
+
+    // screen settings
+    private final int originalTileSize = 16;
+    private final int scale = 4;
+
+    private final int tileSize = originalTileSize * scale;
+    private final int maxScreenCol = 5;
+    private final int maxScreenRow = 5;
+    private final int screenWidth = tileSize * maxScreenCol;
+    private final int screenHeight = tileSize * maxScreenRow;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true); //drawing is done in an offscreen painting buffer thereby increasing performance
+    }
+
 }
