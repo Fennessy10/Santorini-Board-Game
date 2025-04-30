a2fc4adc3dc4dd58ed365226e90bed3b82463371
Joshua J. Seeger <jsee0010@student.monash.edu>
2025-05-01 01:38:31 +1000

    Add abstract methods for god card

--- santorini/src/com/santorini/GodCard.java
+++ santorini/src/com/santorini/GodCard.java
@@ -6,12 +6,20 @@ public abstract class GodCard {
     private String name;
     private String desc;
 
+    // init attributes
     public GodCard(String name, String desc) {
         this.name=name;
         this.desc=desc;
     }
 
-
+    // abstract method will be used to check if move is allowed on the basis of the God's abilities
+    public abstract boolean isMoveAllowed(Cell originalCell, Cell targetCell);
+    // abstract method for move execution on the basis of god power
+    public abstract boolean move(Cell originalCell, Cell targetCell);
+    // abstract method for checking if build is valid based on god abilities
+    public abstract boolean isBuildAllowed(Cell workerCell, Cell buildCell);
+    // abstract method for execution of build
+    public abstract boolean build(Cell workerCell, Cell buildCell);
     // getters&setters
     public String getName() {
         return this.name;
