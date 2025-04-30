2922232e9fde5b56abc106018813c1c91c58641d
Joshua J. Seeger <jsee0010@student.monash.edu>
2025-05-01 01:03:10 +1000

    add more setters to worker & begin work on player class

--- santorini/src/com/santorini/Worker.java
+++ santorini/src/com/santorini/Worker.java
@@ -30,4 +30,15 @@ public class Worker {
         return worker_id;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public void setWorkerId(int worker_id) {
+        this.worker_id = worker_id;
+    }
+
+
 }
