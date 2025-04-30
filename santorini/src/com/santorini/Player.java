28172bc555136c1a8f14d6870632b76f11a29c24
Joshua J. Seeger <jsee0010@student.monash.edu>
2025-05-01 01:05:05 +1000

    add attributes and constructor for player class

--- santorini/src/com/santorini/Player.java
+++ santorini/src/com/santorini/Player.java
@@ -1,4 +1,15 @@
 package com.santorini;
 
 public class Player {
+    private String name;
+    private Worker worker_1;
+    private Worker worker_2;
+    private Worker utilised_worker;
+
+    public Player(String name) {
+        this.name=name;
+        this.worker_1=null;
+        this.worker_2=null;
+        this.utilised_worker=null;
+    }
 }
