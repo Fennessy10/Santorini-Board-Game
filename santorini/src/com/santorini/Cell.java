9a4eaf3a4a6e66f09ece78853d43e25adf65f23a
Joshua J. Seeger <jsee0010@student.monash.edu>
2025-05-01 00:27:18 +1000

    add cell class & begin adding required attributes

---
+++ santorini/src/com/santorini/Cell.java
@@ -0,0 +1,13 @@
+package com.santorini;
+
+import javax.smartcardio.Card;
+
+public class Cell {
+    public int x;
+    public int y;
+    private Worker worker;
+    private boolean has_dome;
+    private int build_lvl;
+
+    
+}
--- santorini/src/com/santorini/SpriteManager.java
+++ santorini/src/com/santorini/SpriteManager.java
@@ -1,5 +1,8 @@
 package main;
 
+
+// Handles sprites (game image assets) for the game
+// Implementation isn't critical at this point. Will be looked at once more important classes are implemented.
 public class SpriteManager {
 
 }
