a5e5b78cf523d805095bd924e0ef2332059a4894
Joshua J. Seeger <jsee0010@student.monash.edu>
2025-04-30 18:22:55 +1000

    Move UI changes to new project

--- santorini/src/com/santorini/WelcomeFrame.java
+++ santorini/src/com/santorini/WelcomeFrame.java
@@ -1,10 +1,17 @@
+
 package com.santorini;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class WelcomeFrame {
+import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
+
+//import static sun.tools.jconsole.inspector.XDataViewer.dispose;
+
+
+public class WelcomeFrame  {
 
 
     private JPanel panel1;
@@ -14,17 +21,18 @@ public class WelcomeFrame {
         btn_Enter.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Log in success" );
+                GamePanelFrame gamePanelFrame = new GamePanelFrame();
+                gamePanelFrame.setVisible(true);
             }
         });
     }
-
     public static void main(String[]args){
-        JFrame newFrame = new JFrame("Login");
+        JFrame newFrame = new JFrame("Welcome");
         newFrame.setContentPane(new WelcomeFrame().panel1);
-        newFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        newFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         newFrame.setVisible(true);
-        newFrame.setSize(500, 500);
+        newFrame.setSize(1000, 1000);
 
     }
 }
+
