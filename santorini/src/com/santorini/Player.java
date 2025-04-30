028091d592015a61d8da732a561288dee5b51174
Joshua J. Seeger <jsee0010@student.monash.edu>
2025-05-01 01:27:43 +1000

    player getters&setters and implement method to grab worker by id

--- santorini/src/com/santorini/Player.java
+++ santorini/src/com/santorini/Player.java
@@ -5,11 +5,56 @@ public class Player {
     private Worker worker_1;
     private Worker worker_2;
     private Worker utilised_worker;
+    // need to implement placeholder atm
+    private GodCard godCard;
 
+    // init class
     public Player(String name) {
         this.name=name;
         this.worker_1=null;
         this.worker_2=null;
         this.utilised_worker=null;
+        this.godCard=null;
     }
+
+    // method grabs the worker with the given id num
+    public fetchWorkerById(int id) {
+        if (id==1) {
+            return this.worker_1;
+        } else if (id==2) {
+            return this.worker_2;
+        }
+            return null;
+
+    }
+
+    // getters&setters
+    public String getName() {
+        return this.name;
+    }
+    public Worker getWorker1() {
+        return this.worker_1;
+    }
+    public void setWorker1(Worker worker1) {
+        this.worker_1 = worker1;
+    }
+    public Worker getWorker2() {
+        return this.worker_2;
+    }
+    public void setWorker2(Worker worker2) {
+        this.worker_2 = worker2;
+    }
+    public GodCard getGodCard() {
+        return this.godCard;
+    }
+    public void setGodCard(GodCard godCard) {
+        this.godCard = godCard;
+    }
+    public Worker getUtilisedWorker() {
+        return utilised_worker;
+    }
+    public void setUtilisedWorker(Worker utilised_worker) {
+        this.utilised_worker = utilised_worker;
+    }
+
 }
