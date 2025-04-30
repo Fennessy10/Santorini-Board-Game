2bb56bd922aabdfef6af66452a20e29b3e20eb8e
Joshua J. Seeger <jsee0010@student.monash.edu>
2025-05-01 02:01:36 +1000

    Create a method to generate cell graphics

--- santorini/src/com/santorini/Board.java
+++ santorini/src/com/santorini/Board.java
@@ -1,5 +1,7 @@
 package com.santorini;
 
+import java.awt.*;
+
 public class Board {
     // attribute for board double array bcuz grid of cells = board
     private Cell[][] cells;
@@ -34,7 +36,39 @@ public class Board {
 
     // checks if coords are within bounds of board
     public boolean isPosAllowed(int x, int y) {
+        // this condition checks that coords are less than the board size and greater than zero
         return x<BOARD_SIZE&&y<BOARD_SIZE&&x>=0&&y>=0;
+    }
+
+    // Method's purpose is for creating rudimentary graphics to differentiate between the different lvls
+    // Will probably replace this testing method with actual graphics using SpriteManager if we have time
+    public void createCellLvlGraphics(int x, int y, int w, int h, Cell cell, Graphics graphics) {
+        Color colour;
+        // Assign colour based on the build level of the cell for differentiation.
+        switch (cell.getBuildLvl()) {
+            case 0:
+                colour = Color.WHITE;
+                break;
+            case 1:
+                colour = Color.YELLOW;
+                break;
+            case 2:
+                colour = Color.GREEN;
+                break;
+            case 3:
+                colour = Color.BLUE;
+                break;
+            default:
+                colour=Color.LIGHT_GRAY;
+        }
+        graphics.setColor(colour);
+        // create rectangle with colour, represents build level for the meantime
+        graphics.fillRect(x+1, y+1, w-2, h-2);
+        graphics.setColor(Color.BLACK);
+        if (cell.getBuildLvl()>0) {
+            // Write text for the level as more clear visual indication. Could help for people that are colourblind as well I guess.
+            graphics.drawString("Lvl"+cell.getBuildLvl(), x+2+3, y+5+10);
+        }
     }
 
 
