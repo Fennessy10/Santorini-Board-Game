dbe00377db5987b692403b07011559d25ed76029
Joshua J. Seeger <jsee0010@student.monash.edu>
2025-05-01 01:01:15 +1000

    add setPos method & begin implement getters

--- santorini/src/com/santorini/Worker.java
+++ santorini/src/com/santorini/Worker.java
@@ -6,11 +6,28 @@ public class Worker {
     private int worker_id;
 
 
-
     // contructor for worker. Init all attributes.
     public Worker(int x, int y, int worker_id) {
         this.x=x;
         this.y=y;
         this.worker_id=worker_id;
     }
+
+    // set position of worker
+    public void setPos(int x, int y) {
+        this.x=x;
+        this.y=y;
+    }
+
+    // get&setters
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public int getWorkerId() {
+        return worker_id;
+    }
+
 }
