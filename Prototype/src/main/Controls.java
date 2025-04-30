e65dca6b6a04b07c901b51ee079a12d5127fe198
pfen0005 <pfen0005@student.monash.edu>
2025-04-30 18:22:46 +1000

    created controls class

---
+++ Prototype/src/main/Controls.java
@@ -0,0 +1,63 @@
+package main;
+
+import java.awt.event.*;
+
+public class Controls implements KeyListener, MouseListener, MouseMotionListener {
+
+    private boolean mousePressed;
+//    public boolean workerPressed;
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        mousePressed = true;
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        mousePressed = false;
+    }
+
+    public boolean MouseIsPressed() {
+        return mousePressed;
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
--- Prototype/src/main/GamePanel.java
+++ Prototype/src/main/GamePanel.java
@@ -1,6 +1,7 @@
+package main;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 
 public class GamePanel extends JPanel implements Runnable {
 
@@ -17,12 +18,19 @@ public class GamePanel extends JPanel implements Runnable {
     private final int screenWidth = tileSize * maxScreenCol;
     private final int screenHeight = tileSize * maxScreenRow;
 
+    Controls controls = new Controls();
     Thread gameThread;
+
+    // set workers default position
+    private int workerX = 100;
+    private int workerY = 100;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true); //drawing is done in an offscreen painting buffer thereby increasing performance
+        this.addKeyListener(controls);
+        this.setFocusable(true); // focused to recieve key input
     }
 
     public void startGameThread() {
@@ -37,5 +45,31 @@ public class GamePanel extends JPanel implements Runnable {
 
         // TODO: draw the board on screen
 
+        while (gameThread != null) {
+            update();
+            repaint();
+        }
+
+
     }
+    public void update() {
+        // if worker clicked
+        if (controls.MouseIsPressed()) {
+            // light up the adjacent spaces around thw worker
+
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.white);
+
+        g2.fillRect(workerX, workerY, tileSize, tileSize);
+
+        g2.dispose(); //release once done (safer + better performance)
+    }
+
+
 }
