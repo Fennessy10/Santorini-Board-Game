e1d55f3576cb6db1ac0dbe0f643c1ff450720b9a
Joshua J. Seeger <jsee0010@student.monash.edu>
2025-05-01 00:59:06 +1000

    begin work on worker class
    add attributes & constructor

---
+++ santorini/src/com/santorini/Worker.java
@@ -0,0 +1,16 @@
+package com.santorini;
+
+public class Worker {
+    private int x;
+    private int y;
+    private int worker_id;
+
+
+
+    // contructor for worker. Init all attributes.
+    public Worker(int x, int y, int worker_id) {
+        this.x=x;
+        this.y=y;
+        this.worker_id=worker_id;
+    }
+}
